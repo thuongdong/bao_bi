20f79449951302d416049c656af5f17dfef392b3
Dong Duc Thuong <thuongdd@lifetimetech.vn>
2020-09-25 17:46:09 +0700

    Create themes

---
+++ wp-content/themes/demo_thems/footer.php
@@ -0,0 +1,83 @@
+<section class="footer">
+  <div class="container">
+    <div class="top-text">Follow Me</div>
+        <div class="col-md-12 col-sm-12 links">
+        <ul>
+            <li class="social-icons">
+            <a href="#" data-toggle="tooltip" title="Some tooltip text!">
+                <div class="circle">
+                <i class="fa fa-facebook"></i>
+                </div>
+            </a>
+            <a href="#">
+                <div class="circle">
+                <i class="fa fa-twitter"></i>
+                </div>
+            </a>
+            <a href="#">
+                <div class="circle">
+                <i class="fa fa-google-plus"></i>
+                </div>
+            </a>
+            <a href="#">
+                <div class="circle">
+                <i class="fa fa-linkedin"></i>
+                </div>
+            </a>
+            <a href="#">
+                <div class="circle">
+                <i class="fa fa-pinterest"></i>
+                </div>
+            </a>
+            </li>
+        </ul>
+        </div>
+
+        <div class="col-md-12 col-sm-12 copyright">
+        <h6>&copy; MePro 2015 All right reserved. Designed by Suave Digital</h6>
+        </div>
+    </div><!--/.container -->
+</section>
+<!-- JavaScripts -->
+<script src="../javascripts/jquery.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js"></script>
+    <script src="../javascripts/bootstrap.min.js"></script>
+    <script src="../javascripts/typed.min.js"></script>
+    <script src="../javascripts/jquery.parallax-1.1.3.js"></script>
+    <script src="../javascripts/owl.carousel.js"></script>
+    <script src="../javascripts/isotope.pkgd.min.js"></script>
+    <script src="../javascripts/jquery.countTo.js"></script>
+    <script src="../javascripts/wow.min.js"></script>
+    <script src="../javascripts/slick.js"></script>
+    <script src="../javascripts/jquery.appear.min.js"></script>
+    <script src="../javascripts/masonry.pkgd.min.js"></script>
+    <script src="../javascripts/custom-map.js"></script>
+    <script src="../javascripts/custom.js"></script>
+
+    <!-- Style Switcher - Demo Only Begin -->
+    <div class="switcher-box">
+      <div class="colorbox-container" style="display: none">
+        <div class="head"><i class="pe-7s-paint"></i></div>
+        <div class="content">
+          <a title="blue" href="javascript:void(0)">
+            <div class="colorbox" id="blue"></div>
+          </a>
+          <a title="green" href="javascript:void(0)">
+            <div class="colorbox" id="green"></div>
+          </a>
+          <a title="orange" href="javascript:void(0)">
+            <div class="colorbox" id="orange"></div>
+          </a>
+          <a title="yellow" href="javascript:void(0)">
+            <div class="colorbox" id="yellow"></div>
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <script src="../javascripts/switcher.js"></script>
+    <!-- Style Switcher - Demo Only End -->
+
+  </body>
+<?php wp_footer();?>
+</html>
---
+++ wp-content/themes/demo_thems/header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<!-- Mirrored from demo.suavedigital.com/MePro/ by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 23 Jul 2015 15:38:39 GMT -->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Diego - Responsive One Page Template">
+    <link rel="icon" href="assets/images/favicon.png">
+    <title>MePro - Creative Personal Portfolio Template</title>
+
+    <!-- Stylesheets -->
+
+    <!-- Bootstrap -->
+    <link href="<?php echo get_template_directory_uri(); ?>/bootstrap.css" rel="stylesheet">
+
+    <!-- font-awesome.css -->
+    <link href="<?php echo get_template_directory_uri(); ?>/font-awesome.css" rel="stylesheet">
+
+    <!-- pe-icon.css -->
+    <link href="<?php echo get_template_directory_uri(); ?>/pe-icon-7-stroke.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/helper.css" rel="stylesheet">
+
+    <!-- Animate.css -->
+    <link href="<?php echo get_template_directory_uri(); ?>/animate.css" rel="stylesheet">
+
+    <!-- Owl Carousel -->
+    <link href="<?php echo get_template_directory_uri(); ?>/owl-carousel/owl.carousel.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/owl-carousel/owl.theme.css" rel="stylesheet">
+
+    <!-- Slick -->
+    <link href="<?php echo get_template_directory_uri(); ?>/slick/slick.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/slick/slick-theme.css" rel="stylesheet">
+
+    <!-- Main CSS -->
+    <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
+    <!-- <link href="assets/stylesheets/css/color-scheme.css" rel="stylesheet"> -->
+
+    <!-- Font style live -->
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Courgette' rel='stylesheet' type='text/css'>
+
+    <!-- Style Switcher -->
+    <link href="<?php echo get_template_directory_uri(); ?>/switcher.css" rel="stylesheet">
+    <link href="<?php echo get_template_directory_uri(); ?>/yellow.css" id="switch_style" rel="stylesheet"/>
+    <?php wp_head();?>
+</head>
